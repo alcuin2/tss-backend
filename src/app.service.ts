146f0421c914a38516ab3a1a16e990ead505515f
Oke Agi-Tuedor <mail2oke@gmail.com>
2021-02-20 10:47:20 +0100

    home instructions

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello TSS! See documentation at https://documenter.getpostman.com/view/5539822/TWDXmwEi';
   }
 }
